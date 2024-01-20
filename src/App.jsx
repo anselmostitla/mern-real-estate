fd5ff0109cf3194ea0ad3fd4fd25b90c50e13bb1
anselmo <anselmostitla@gmail.com>
2024-01-20 17:22:50 -0600

    install reac.js and tailwindcss and create the first template

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+
+
+// function App() {
+  
+
+//   return (
+//     <>
+//     append, insert, extend, list, split, arr.copy, arr[:], index, sort, reverse, pop(i), remove, clear, 
+//     </>
+//   )
+// }
+
+// export default App
+
+
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      "".join(py_arr)
+    </div>
+  )
+}
+
